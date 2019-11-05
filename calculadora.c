7c60ebb48ea7687ad6edbffe145d4731c4c9ee35
MonteiroLuis <57410261+MonteiroLuis@users.noreply.github.com>
2019-11-05 14:30:26 -0300

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main{
+float a, b, c, d, e, f;
+
+printf("Quatro cálculos básicos");
+printf("\nDigite dois números: ");
+scanf("%f", a);
+printf(" e ");
+scanf("%f", b);
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+printf(".\n%.2f+%.2f=%.2f", a, b, c, "\n%.2f-%.2f=%.2f", a, b, d, "\n%.2f*%.2f=%.2f", a, b, e, "\n%.2f/%.2f=%.2f", a, b, f);
+}
